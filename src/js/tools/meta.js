58a463d5259b9d6f6967998d307d9b0e5092f216
Kent Erik <kent.erik.hole@outlook.com>
2023-05-24 16:05:29 +0200

    adding meta tage

--- src/js/tools/meta.js
+++ src/js/tools/meta.js
@@ -4,6 +4,10 @@
  * Change Meta Tag
  * @param {string} MetaTag 
  * @param {String} Content 
+ * 
+ * @example
+ * ```JS
+ * // Change MetaTage
  */
 export const SetMetaTAG = (MetaTag, Content) => {
     document.querySelector(`meta[name="${MetaTag}"]`).setAttribute("content", `${Content}`)
