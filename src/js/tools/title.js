1285bbae8aa883a169f190a3a107a332f9565952
Kent Erik <kent.erik.hole@outlook.com>
2023-05-25 11:11:04 +0200

    add title

--- src/js/tools/title.js
+++ src/js/tools/title.js
@@ -5,6 +5,16 @@
  * @param {String} NewTitle 
  */
 
+/**
+ * @param {string and  Number} NewTitle
+ * 
+ * 
+ * @example
+ * ```
+ * 
+ * ```
+ */
+
 export const SetTitle = (NewTitle) => {
     document.title = NewTitle;
 }
