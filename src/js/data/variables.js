58456c15595e5e60b2ad10cb68ea4f0d27b6601d
Kent Erik <kent.erik.hole@outlook.com>
2023-05-25 12:46:00 +0200

    adding endpoint

--- src/js/data/variables.js
+++ src/js/data/variables.js
@@ -2,4 +2,7 @@
     // Base URL
         const base_url = 'https://frontendkenterik.no/wp-json/'
     // End Point 
-        
+        // Media 
+            const media = 'wp/v2/media'
+        // Tag 
+            const tag = '/wp/v2/tags'
