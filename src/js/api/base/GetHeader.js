1b4c195e470433edff18ce7bf0b521a9b8e55ce7
Kent Erik <kent.erik.hole@outlook.com>
2023-07-02 16:43:03 +0200

    adding api data

--- src/js/api/base/GetHeader.js
+++ src/js/api/base/GetHeader.js
@@ -1,8 +1,12 @@
+import {api} from '../../data/variables'
 
+// https://mamsenapi.frontendkenterik.no/wp-json/wp/v2/posts?categories=3
 export async function GetHeader() {
+    const {base_URL, posts, media} = api 
     try {
+        
         const response = await fetch(
-            'https://mamsenapi.frontendkenterik.no/wp-json/wp/v2/posts?categories=3',
+           `${base_URL}${posts}?categories=3`,
             {
                 method: 'GET',
                 headers: {
@@ -13,7 +17,7 @@ export async function GetHeader() {
         console.log(response)
         if(response.ok) {
             const data = await response.json()
-            
+            console.log(data)
         }
         if(!response.ok){
             if(response.status === 400) {
